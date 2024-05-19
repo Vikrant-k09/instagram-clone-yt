0f1c08082b174a9cb8010cc367df239c233eea3d
Vikrant <94465004+Vikrant-k09@users.noreply.github.com>
2024-05-19 20:20:10 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/firestore"
-import {getStorage} from "firebase/storage"
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAE3DakH2zbq5OEvYc1HyZ8n1F4TWZk7U8",
   authDomain: "instagram-a6299.firebaseapp.com",
@@ -19,7 +16,7 @@ const firebaseConfig = {
   measurementId: "G-MGF3T7T4NN"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
